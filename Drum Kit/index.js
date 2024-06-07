2383ce2dc32381bb861638096849ac5c135bb5b5
Nikhila Lanke <115094180+nikhila-lanke@users.noreply.github.com>
2024-06-07 21:09:45 +0530

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -3,12 +3,14 @@ for (var i=0;i<numberOfButtons; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 }
 );
 }
 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -44,4 +46,13 @@ function makeSound(key){
         default:
             break;
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
